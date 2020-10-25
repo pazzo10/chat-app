b5bcb2180e7120e62e2d8a807bba4bf42f1481ef
pazzo10 <patrickrene.mwimanzi@gmail.com>
2020-10-25 22:15:49 +0100

    quet 4 done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,23 +2,21 @@ import React from 'react';
 import './contact.css';
 
 
-const avatar = "https://randomuser.me/api/portraits/lego/2.jpg";
-const name = "Patrick Mwimanzi";
-
-function Contact () {
-    return (
-      <article className="Contact">
-          <img className="avatar" src={avatar} alt="my profile"/>
-          <div>
-            <h2 className="name">{name}</h2>      
-            <button className="status">
-              <span class="status-online" ></span>
-              <p class="status-text">Online</p>
-            </button>
-          </div>
-      </article>
-
-    );
+function Contact(props) {
+  return (
+    <article className="Contact">
+      <img className="avatar" src={props.avatar} alt="{props.name}" />
+      <div>
+        <h2 className="name">{props.name}</h2>
+        <button className="status">
+          <span
+            className={props.online ? "status-online" : "status-offline"}
+          ></span>
+          <p className="status-text">{props.online ? "online" : "offline"}</p>
+        </button>
+      </div>
+    </article>
+  );
 }
 
 export default Contact;
